9457891fdc0bcd14d29a3063ced19e7d218e683a
Alexandra Côté-Lahue <111417596+itsyagirlalexcote@users.noreply.github.com>
2022-12-05 16:44:40 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const numOfDrinks = 5;
+
+const drinks = function () {
+  const tea = 6 + numOfDrinks;
+  console.log(tea);
+};
+
+drinks();
+// 11
+
+if (numOfDrinks === 5) {
+  let soda = "lemon-lime";
+  console.log(soda);
+}
